428e184e35060ef8b41eb4e391e7ab6e67a4f98b
LeonTG77 <leontg77@gmail.com>
2015-09-04 19:09:55 +0200

    Clearned up the code and removed moved methods

--- src/com/leontg77/timer/Main.java
+++ src/com/leontg77/timer/Main.java
@@ -2,90 +2,45 @@ package com.leontg77.timer;
 
 import java.util.logging.Logger;
 
-import net.minecraft.server.v1_8_R3.IChatBaseComponent;
-import net.minecraft.server.v1_8_R3.IChatBaseComponent.ChatSerializer;
-import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
-
-import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.leontg77.timer.cmds.TimerCommand;
+
 /**
  * Main class of the UHC plugin.
+ * <p>
+ * This class contains methods for enabling, disabling and getting the prefix.
+ * 
  * @author LeonTG77
  */
 public class Main extends JavaPlugin {
-	private final Logger logger = Bukkit.getServer().getLogger();
+	private Logger logger = getLogger();
 	public static Main plugin;
-	
-	private static final long SECONDS_PER_HOUR = 3600;
-	private static final long SECONDS_PER_MINUTE = 60;
 	
 	@Override
 	public void onDisable() {
-		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info(pdfFile.getName() + " is now disabled.");
+		PluginDescriptionFile file = getDescription();
+		logger.info(file.getName() + " is now disabled.");
+		
 		plugin = null;
 	}
 	
 	@Override
 	public void onEnable() {
-		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info(pdfFile.getName() + " v" + pdfFile.getVersion() + " is now enabled.");
-		
+		PluginDescriptionFile file = getDescription();
+		logger.info(file.getName() + " v" + file.getVersion() + " is now enabled.");
+		logger.info(file.getName() + " was made by LeonTG77.");
 		plugin = this;
+
 		getCommand("timer").setExecutor(new TimerCommand());
 	}
 	
 	/**
-	 * Get the UHC prefix with an ending color.
-	 * @param endcolor the ending color.
-	 * @return The UHC prefix.
+	 * Get the timer plugin prefix.
+	 * @return The timer prefix.
 	 */
 	public static String prefix() {
-		String prefix = "§c§lTimer §8§l>> §7";
-		return prefix;
-	}
-	
-	/**
-     * Converts the seconds to human readable
-     * @param ticks the  number of ticks
-     * @return the human readable version
-     */
-    public static String ticksToString(long ticks) {
-        int hours = (int) Math.floor(ticks / (double) SECONDS_PER_HOUR);
-        ticks -= hours * SECONDS_PER_HOUR;
-        int minutes = (int) Math.floor(ticks / (double)SECONDS_PER_MINUTE);
-        ticks -= minutes * SECONDS_PER_MINUTE;
-        int seconds = (int) ticks;
-
-        StringBuilder output = new StringBuilder();
-        if (hours > 0) {
-            output.append(hours).append('h');
-            if (minutes == 0) {
-            	output.append("0m");
-            }
-        }
-        if (minutes > 0) {
-            output.append(minutes).append('m');
-        }
-        output.append(seconds).append('s');
-
-        return output.toString();
-    }
-    
-    /**
-	 * Sends a message in action bar to a player.
-	 * @param player the player.
-	 * @param msg the message.
-	 */
-	public static void sendAction(Player player, String msg) {
-		CraftPlayer p = (CraftPlayer) player;
-        
-        IChatBaseComponent cbc = ChatSerializer.a("{text:'" + msg + "'}");
-        PacketPlayOutChat ppoc = new PacketPlayOutChat(cbc, (byte) 2);
-        p.getHandle().playerConnection.sendPacket(ppoc);
+		return "§c§lTimer §8» §7";
 	}
 }
