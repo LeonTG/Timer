f4de4471d36542d2cd6fff697ff375a407b7996b
Graham <graham_howden1@yahoo.co.uk>
2015-09-05 11:18:57 +0100

    command executor only gets called for a command if it is set to

--- src/main/java/com/leontg77/timer/cmds/TimerCommand.java
+++ src/main/java/com/leontg77/timer/cmds/TimerCommand.java
@@ -30,94 +30,93 @@ public class TimerCommand implements CommandExecutor {
 	}
 
 	public boolean onCommand(final CommandSender sender, Command cmd, String label,String[] args) {
-		if (cmd.getName().equalsIgnoreCase("timer")) {
-			if (sender.hasPermission("uhc.timer")) {
-				if (args.length == 0) {
-					sender.sendMessage(Main.PREFIX + "Usage: /timer <seconds> <message>");
-					return true;
-				}
-				
-				if (args.length == 1 && !args[0].equalsIgnoreCase("cancel")) {
-					sender.sendMessage(Main.PREFIX + "Usage: /timer <seconds> <message>");
-					return true;
-				}
+		if (sender.hasPermission("uhc.timer")) {
+			if (args.length == 0) {
+				sender.sendMessage(Main.PREFIX + "Usage: /timer <seconds> <message>");
+				return true;
+			}
 
-				if (args.length >= 1 && args[0].equalsIgnoreCase("cancel")) {
-					if (run == null) {
-						sender.sendMessage(ChatColor.RED + "No timers are running.");
-						return true;
-					}
+			if (args.length == 1 && !args[0].equalsIgnoreCase("cancel")) {
+				sender.sendMessage(Main.PREFIX + "Usage: /timer <seconds> <message>");
+				return true;
+			}
 
-					run.cancel();
-					run = null;
-					
-					sender.sendMessage(Main.PREFIX + "Timer cancelled.");
+			if (args.length >= 1 && args[0].equalsIgnoreCase("cancel")) {
+				if (run == null) {
+					sender.sendMessage(ChatColor.RED + "No timers are running.");
 					return true;
 				}
 
-				if (run != null) {
-					sender.sendMessage(ChatColor.RED + "A timer is already running.");
-					return true;
-				}
-				
-				int time;
+				run.cancel();
+				run = null;
 
-				try {
-					time = Integer.parseInt(args[0]);
-				} catch (Exception e) {
-					sender.sendMessage(ChatColor.RED + "Invaild time.");
-					return true;
-				}
-				
-				if (time < 0) {
-					countdown = false;
-				} else {
-					countdown = true;
-				}
+				sender.sendMessage(Main.PREFIX + "Timer cancelled.");
+				return true;
+			}
 
-				StringBuilder msg = new StringBuilder();
-				
-				for (int i = 1; i < args.length; i++) {
-					msg.append(args[i]).append(" ");
-				}
-				
-				run = new BukkitRunnable() {
-					public void run() {
-						if (countdown) {
-							ticks--;
+			if (run != null) {
+				sender.sendMessage(ChatColor.RED + "A timer is already running.");
+				return true;
+			}
 
-							try {
-								actionSender.sendToAll(message + " " + TimeUtils.timeToString(ticks));
-							} catch (Exception ex) {
-								ex.printStackTrace();
-								plugin.getLogger().severe("Could not send action packets, are you using 1.8 or higher?");
-							}
-							
-							if (ticks == 0) {
-								cancel();
-								run = null;
-								return;
-							}
-						} else {
-							try {
-								actionSender.sendToAll(message + " " + TimeUtils.timeToString(ticks));
-							} catch (Exception ex) {
-								ex.printStackTrace();
-								plugin.getLogger().severe("Could not send action packets, are you using 1.8 or higher?");
-							}
+			int time;
+
+			try {
+				time = Integer.parseInt(args[0]);
+			} catch (Exception e) {
+				sender.sendMessage(ChatColor.RED + "Invaild time.");
+				return true;
+			}
+
+			if (time < 0) {
+				countdown = false;
+			} else {
+				countdown = true;
+			}
+
+			StringBuilder msg = new StringBuilder();
+
+			for (int i = 1; i < args.length; i++) {
+				msg.append(args[i]).append(" ");
+			}
+
+			run = new BukkitRunnable() {
+				public void run() {
+					if (countdown) {
+						ticks--;
+
+						try {
+							actionSender.sendToAll(message + " " + TimeUtils.timeToString(ticks));
+						} catch (Exception ex) {
+							ex.printStackTrace();
+							plugin.getLogger().severe("Could not send action packets, are you using 1.8 or higher?");
+						}
+
+						if (ticks == 0) {
+							cancel();
+							run = null;
+							return;
+						}
+					} else {
+						try {
+							actionSender.sendToAll(message + " " + TimeUtils.timeToString(ticks));
+						} catch (Exception ex) {
+							ex.printStackTrace();
+							plugin.getLogger().severe("Could not send action packets, are you using 1.8 or higher?");
 						}
 					}
-				};
-				run.runTaskTimer(plugin, 0, 20);
-				
-				this.message = ChatColor.translateAlternateColorCodes('&', msg.toString().trim());
-				this.ticks = (time + 1);
-				
-				sender.sendMessage(Main.PREFIX + "Timer started.");
-			} else {
-				sender.sendMessage(Main.PREFIX + "You can't use that command.");
-			}
+				}
+			};
+			run.runTaskTimer(plugin, 0, 20);
+
+			this.message = ChatColor.translateAlternateColorCodes('&', msg.toString().trim());
+			this.ticks = (time + 1);
+
+			sender.sendMessage(Main.PREFIX + "Timer started.");
+		} else {
+			sender.sendMessage(Main.PREFIX + "You can't use that command.");
 		}
+
 		return true;
 	}
 }
