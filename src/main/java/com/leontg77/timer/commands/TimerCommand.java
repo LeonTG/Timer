e7ad8342097c35aa0bb69c9cb43bd51155c19ef8
LeonTG77 <leontg77@gmail.com>
2017-01-14 15:22:24 +0100

    Added license headers

--- src/main/java/com/leontg77/timer/commands/TimerCommand.java
+++ src/main/java/com/leontg77/timer/commands/TimerCommand.java
@@ -1,18 +1,48 @@
+/*
+ * Project: ActionTimer
+ * Class: com.leontg77.timer.commands.TimerCommand
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Leon Vaktskjold <leontg77@gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.leontg77.timer.commands;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import com.leontg77.timer.Main;
 import com.leontg77.timer.managers.TimerRunnable;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 import org.bukkit.util.StringUtil;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Timer command class.
@@ -26,7 +56,7 @@ public class TimerCommand implements CommandExecutor, TabCompleter {
         this.timer = timer;
     }
 
-    private static final String PERMISSION = "uhc.timer";
+    private static final String PERMISSION = "timer.manage";
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label,String[] args) {
@@ -82,18 +112,21 @@ public class TimerCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
-        List<String> toReturn = Lists.newArrayList();
-
-        if (args.length != 1) {
-            return toReturn;
+        if (!sender.hasPermission(PERMISSION)) {
+            return null;
         }
 
-        toReturn.add("info");
+        List<String> toReturn = Lists.newArrayList();
 
-        if (sender.hasPermission(PERMISSION)) {
-            toReturn.add("enable");
-            toReturn.add("start");
-            toReturn.add("disable");
+        if (args.length == 1) {
+            toReturn.add("cancel");
+        }
+
+        if (args.length == 1) {
+            toReturn.addAll(Bukkit.getOnlinePlayers()
+                    .stream()
+                    .map(Player::getName)
+                    .collect(Collectors.toList()));
         }
 
         return StringUtil.copyPartialMatches(args[args.length - 1], toReturn, Lists.newArrayList());
